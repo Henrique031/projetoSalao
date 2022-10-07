1fd6ac56d0fc0b47720dd845f5051ab9c73882dd
Henrique Galdino <102200241+Henrique031@users.noreply.github.com>
2022-10-07 13:53:45 -0300

    Upgrade

--- src/redes.js
+++ src/redes.js
@@ -5,11 +5,10 @@ import Estilo from './estilos'
 export default props => {
   return (
     <View style={Estilo.viewRedes} >
-      <Text > {props.rede1} </Text> 
-      <Text > {props.rede2} </Text> 
-      <Text > {props.rede3} </Text> 
+      <Text > {props.rede1} </Text>
+      <Text > {props.rede2} </Text>
+      <Text > {props.rede3} </Text>
       <Text > {props.rede4} </Text> 
     </View>
   )
-  
 }
